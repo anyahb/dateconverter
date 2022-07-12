2a0f860f5b62702e47d0736919530b70f6260e94
Анна Артемьева <artemiden@macbook-pro.myfiosgateway.com>
2022-07-12 01:30:32 -0400

    inputHandler function: condition added, 'London' part of a 'converting' function: changing time if the button is clicked

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,30 +16,15 @@ export const Search = () => {
     const [convertedTime, setConvertedTime] = useState('')
     const [currentConvertedCity, setCurrentConvertedCity] = useState("Moscow") // текущий город в который комментируем
     const [lond, modified] = useState('')
-    /*const [counter,setCounter] = useState(0)
-    const [count, changed] = useState(date.toTimeString())*/
+
     const offsetMoscow = 7
     const offsetLondon = 5
 
     /* useEffect(() => {
          console.log('counter changed')
      },[])*/
-    const inputHandler = (event) => {
-        const value = event.target.value
-        setCurrentTime(value)
 
 
-        // должна быть проверка. Если выбран текущий город - Москва, то меняется на мск. Если лондон то на Лондон
-
-        // const moscow = value.split(':')
-        //
-        //
-        // if (moscow[0] <= 17) {
-        //     changed(parseInt(moscow[0]) + offset + ":" + moscow[1])
-        // } else {
-        //     changed(offset - (24 - parseInt(moscow[0])) + ":" + moscow[1])
-        // }
-    }
 
     const moscowTime = function () {
         const valMoscow = currentTime
@@ -64,8 +49,7 @@ export const Search = () => {
     }
 
     const converting = function (city) { // Moscow
-        setCurrentConvertedCity(city) // Moscow
-
+        setCurrentConvertedCity(city)
         if (city === "Moscow") {
             const moscva = currentTime.split(':')
             if (moscva[0] <= 17) {
@@ -74,6 +58,38 @@ export const Search = () => {
                 setConvertedTime(offsetMoscow - (24 - parseInt(moscva[0])) + ":" + moscva[1])
             }
         }
+        if (city === "London") {
+            const london = currentTime.split(':')
+            if (london[0] <= 17) {
+                setConvertedTime(parseInt(london[0]) + offsetLondon + ":" + london[1])
+            } else {
+                setConvertedTime(offsetLondon - (24 - parseInt(london[0])) + ":" + london[1])
+            }
+        }
+
+    }
+
+    const inputHandler = (event) => {
+        const value = event.target.value
+        const conv = currentConvertedCity
+        if (conv === "Moscow"){
+            const moscva = value.split(':')
+            if (moscva[0] <= 17) {
+                setCurrentTime(parseInt(moscva[0]) + offsetMoscow + ":" + moscva[1])
+            } else {
+                setCurrentTime(offsetMoscow - (24 - parseInt(moscva[0])) + ":" + moscva[1])
+            }
+        }
+
+        if (conv === "London"){
+            const london = value.split(':')
+            if (london[0] <= 17) {
+                setCurrentTime(parseInt(london[0]) + offsetLondon + ":" + london[1])
+            } else {
+                setCurrentTime(offsetLondon - (24 - parseInt(london[0])) + ":" + london[1])
+            }
+        }
+
     }
 
 
@@ -83,14 +99,15 @@ export const Search = () => {
 
             <input placeholder={'type your time:'} onInput={inputHandler}/>
 
-
             <h1>{currentTime}</h1>
 
             <button onClick={() => converting("Moscow")}>Moscow</button>
             <button onClick={() => converting("London")}>London</button>
 
-            <h1>currentConvertedCity: {currentConvertedCity}</h1>
+            <h1>{currentConvertedCity}</h1>
             <h1>{convertedTime}</h1>
+
+
             <Main/>
         </div>
     )
